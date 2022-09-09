669106bf5d2089403743b8b9f8c354a110bda4e8
nonny71 <you@michealnonny71@gmail.com>
2022-09-09 04:42:31 -0700

    put

--- 0x00-hello_world/0-puts.c
+++ 0x00-hello_world/0-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 /**
- * main - Main block
- * Return: 0
+ * main - Entry point
+ *Return:Always 0(Process);
  */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
-return (0);
+return(0);
 }
