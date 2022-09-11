1a9745d615fe796c2fd92e0988eee9f456745042
nonny71 <you@michealnonny71@gmail.com>
2022-09-10 20:45:26 -0700

    print

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -6,14 +6,11 @@
  * Return: 0
  */
 int main(void)
-
-   char alphabet;
 {
-   for(alphabet = 'a';alphabet <='z';alphabe++);
-   {
-       putchar(alphabet);
-   }
-   putchar('\n');
-   return (0);
-
+	char alphabet
+		
+	for (alphabet = 'a'; alphabet <= 'z' ; alphabet++)
+		putchar(alphabet);
+	putchar('\n');
+	return (0);
 }
