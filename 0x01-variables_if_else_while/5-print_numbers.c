8b17106578ffb0559acd71f583e068ebd15f358f
nonny71 <you@michealnonny71@gmail.com>
2022-09-11 12:00:20 -0700

    number

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Print all digit number of base 10 from 0
+ *
+ * Return:always 0
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+		printf("%d", num);
+
+	printf("\n");
+	return (0);
+}
