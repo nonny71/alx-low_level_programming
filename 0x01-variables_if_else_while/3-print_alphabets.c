8a1b7712e83cc3b68ad76db712eb14fdae4b4977
nonny71 <you@michealnonny71@gmail.com>
2022-09-11 06:08:33 -0700

    letter

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,13 +9,12 @@ int main(void)
 {
 	char letter;
 
-	for (letter = 'a'; letter  <= 'z'; letter++ )
+	for (letter = 'a'; letter  <= 'z'; letter++)
 		putchar(letter);
 
 	for (letter = 'A'; letter <= 'Z'; letter++)
 		putchar(letter);
 
 	putchar('\n');
-	return(0);
-
+	return (0);
 }
