f8522e068708dc0a20ec771ccd19741e8d5c7f3d
nonny71 <you@michealnonny71@gmail.com>
2022-09-12 09:38:44 -0700

    100 combo

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - Print all posible combination of two numbers
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int x, y;
+
+	for (x = 48; x <= 56; x++)
+	{
+		for (y = 49; y <= 57; y++)
+		{
+			if (y > x)
+			{
+				putchar(x);
+				putchar(y);
+				if (x != 56 || y != 57)
+				{
+				putchar(',');
+				putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
