bae15314637b0e21f7d51e0eaabdfcd965629460
nonny71 <you@michealnonny71@gmail.com>
2022-09-12 02:44:12 -0700

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,9 +12,10 @@ long int l;
 long long int d;
 float f;
 
-printf("Size of a char: %ld byte(s)\n",sizeof(c));
-printf("Size of an int: %ld byte(s)\n",sizeof(i));
-printf("Size of a long int: %ld byte(s)\n",sizeof(l));
-printf("Size of a long long int: %ld byte(s)\n"sizeoff(d);
-printf("Size of a float: %ld byte(s)\n",sizeof(d)
-
+printf("Size of a char: %ld byte(s)\n", sizeof(c));
+printf("Size of an int: %ld byte(s)\n", sizeof(i));
+printf("Size of a long int: %ld byte(s)\n", sizeof(l));
+printf("Size of a long long int: %ld byte(s)\n", sizeof(d);
+printf("Size of a float: %ld byte(s)\n", sizeof(d);
+return (0);
+}
