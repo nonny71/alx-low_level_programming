f42495a8fe7eeddefdcc15ab00106b482221bf60
nonny71 <you@michealnonny71@gmail.com>
2022-09-12 07:14:37 -0700

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Print that piece of art is useful
+ *
+ * Return: Always 1
+ */
+int main(void)
+{
+	write(2,
+		"and that piece of art is useful\" - Dora Korpa, 2015-10-19\n",
+		61);
+	return (1);	
+}
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,7 +2,7 @@
 
 /**
  * main - Main block
- * Return:always 0`
+ * Return:always 0
  */
 int main(void)
 {
