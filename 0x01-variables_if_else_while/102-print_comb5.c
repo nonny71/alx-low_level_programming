158c1ab20685df0ff94d7c4ee2fd0a5ce6e0503a
nonny71 <you@michealnonny71@gmail.com>
2022-09-12 10:12:16 -0700

    102 combo

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -13,10 +13,10 @@ int main(void)
 	{
 		for (num2 = 1; num2 <= 99; num2++)
 		{
-		putchar((num1 \ 10) + '0');
+		putchar((num1 / 10) + '0');
 		putchar((num1 % 10) + '0');
 		putchar(' ');
-		putchar((num2 \ 10) + '0');
+		putchar((num2 / 10) + '0');
 		putchar((num2 % 10) + '0');
 
 		if (num1 == 98 && num2 == 99)
