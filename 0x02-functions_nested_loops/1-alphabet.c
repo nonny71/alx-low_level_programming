08dd9151cc696df22ae3eb2ccb76609de89921e9
nonny71 <you@michealnonny71@gmail.com>
2022-09-13 17:14:19 -0700

    alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,8 +1,17 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
  * main - print alphabet
  *
  * Return: Always 0
  */
+void print_alphabet(void)
+{
+	int n;
 
+	for (n = 'a'; n <= 'z'; n++)
+	{
+		_putchar(n);
+	}
+	_putchar('\n');
+}
