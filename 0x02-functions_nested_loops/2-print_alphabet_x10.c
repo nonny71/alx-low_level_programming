05061fc51e9694626798c55807945b378a483f6e
nonny71 <you@michealnonny71@gmail.com>
2022-09-13 17:46:08 -0700

    alphabetx10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main"
+
+/**
+ * Print_alphabet_x10 - a function that print 10 times the alphabet in lowercase
+ * 
+ *
+ * Return: Always 0
+ */
+void print_alphabet_x10(void)
+{
+	int n, m;
+
+	m = 0;
+
+	while (m < 10)
+	{
+		for (n = 'a'; n <= 'z'; n++)
+		{
+			_putchar(n);
+		}
+		m++;
+		_putchar('\n');
+	}
+}
