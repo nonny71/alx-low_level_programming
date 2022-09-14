c5fbe240b0f641b3c597cdb995482c2defe2b3d8
nonny71 <you@michealnonny71@gmail.com>
2022-09-14 06:20:37 -0700

    print98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -7,7 +7,7 @@
  * @int: the return type of the numbers
  * Return: the last printed number should be 98
  */
-int print_to_98(int n)
+void print_to_98(int n)
 {
 	if (n >= 98)
 	{
@@ -17,8 +17,8 @@ int print_to_98(int n)
 	}
 	else
 	{
-	while (n < 98)
-		printf("%d, ", n++);
-	Printf("%d\n", n);
+		while (n < 98)
+			printf("%d, ", n++);
+		printf("%d\n", n);
 	}
 }
