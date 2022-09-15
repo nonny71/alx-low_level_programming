6a2400f00c5cf6a7e3577f9d5115ceb53a8306dd
nonny71 <you@michealnonny71@gmail.com>
2022-09-14 20:40:44 -0700

    103

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,24 @@
+#iclude "main.h"
+
+/**
+ * main - print the sum of even fibonacci number
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int a = 0;
+	long b, c = 2, sum = c;
+
+	while (c + b < 4000000)
+	{
+		c += b;
+		if (c % 2 == 0)
+			sum += c;
+		b = c - b;
+
+		++a;
+	}
+	printf("%ld\n", sum);
+	return (0);
+}
