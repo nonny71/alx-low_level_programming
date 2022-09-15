b208b2d8f285cf87ac8e381f3ad46ad3e477f0b8
nonny71 <you@michealnonny71@gmail.com>
2022-09-14 20:22:49 -0700

    nature

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - print the sum  of all multiple of 3 and 5 up to 1024
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int a, b = 0;
+
+	while (a < 1024)
+	{
+		if ((a % 3 == 0) || (a % 5 == 0))
+		{
+			b += a
+		}
+		a++
+	}
+	Printf("%d\n", b);
+	return (0);
+}
