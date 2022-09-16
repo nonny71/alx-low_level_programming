b56d5decf2160b3f43ca664b2b1705fad10e0e86
nonny71 <you@michealnonny71@gmail.com>
2022-09-16 04:38:30 -0700

    most

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *print_most_numbers -  prints the numbers, from 0 to 9
+ */
+void print_most_numbers(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+	{
+		for (i != 2 || i != 4)
+			_putchar(i);
+	}
+	_putchar('\n');
+}
