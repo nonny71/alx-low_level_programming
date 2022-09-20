9b2f4dfc7fc344567bbf3fac66e9e48556a0347c
nonny71 <you@michealnonny71@gmail.com>
2022-09-19 18:54:54 -0700

    put

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string, followed by a newline
+ * @str: string parameter to print
+ * Return: Nothing
+ */
+void _puts(char *str)
+{
+	int a;
+
+	for (a = 0; str[a] != '\0'; a++)
+	{
+		_putchar(str[a]);
+	}
+	_putchar('\n')
+}
