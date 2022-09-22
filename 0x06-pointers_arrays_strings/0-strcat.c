a861ea83b48ad3bad14ee4cd3ab2f5196c42ab16
nonny71 <you@michealnonny71@gmail.com>
2022-09-22 14:34:37 -0700

    strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -11,17 +11,13 @@ char *strcat(char *dest, const char *src)
 	int i, j;
 
 	i = 0;
-	while (dest[i] != '\0')
+	while (dest[i])
 	{
 		i++;
 	}
-	j = 0;
-	for (src[j] != '\0')
+	for (j = 0; src[j]; j++)
 	{
-		dest[i] = src[j];
-		i++;
-		j++;
+		dest[i++] = src[j++]
 	}
-	dest[i] != '\0';
 	return (dest);
 }
