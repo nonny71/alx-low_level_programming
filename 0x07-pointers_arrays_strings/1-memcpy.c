4a84e57cb3dd7a6308492cb550e013eade8b8f8b
nonny71 <you@michealnonny71@gmail.com>
2022-09-26 16:24:30 -0700

    memes

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -10,10 +10,8 @@
 void *_memcpy(void *dest, const void *src, size_t n)
 {
 	unsigned int index;
-	unsigned char *destination = dest;
-	const unsigned char *source = src;
 
 	for (index = 0; index < n; index++)
-		destination[index] = source[index];
+		dest[index] = src[index];
 	return (dest);
 }
