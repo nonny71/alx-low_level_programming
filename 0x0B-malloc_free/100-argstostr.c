6c3ef90776245c1f6b3724f18a8e091e9eee4d50
nonny71 <you@michealnonny71@gmail.com>
2022-10-05 07:34:21 -0700

    100-argstostr

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -11,21 +11,21 @@
 char *argstostr(int ac, char **av)
 {
 	char *str;
-	int arg, byte, index, size = 0;
+	int arg, byte, index = 0, size = 0;
 
 	if (ac == 0 || av == NULL)
-		return (NULL);
+		return ('\0');
 
 	for (arg = 0; arg < ac; arg++)
 	{
-		for (byte = 0; av[arg][byte]; byte++)
+		for (byte = 0; av[arg][byte] != '\0'; byte++)
 			size++;
 	}
 	str = malloc(sizeof(char) * size + 1);
 
 	if (str == NULL)
 	{
-		return (NULL);
+		return ('\0');
 	}
 
 	for (arg = 0; arg < ac; arg++)
