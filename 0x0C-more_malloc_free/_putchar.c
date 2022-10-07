cfe5108bea6c20686771651ec2ba9ce03463338e
nonny71 <you@michealnonny71@gmail.com>
2022-10-06 20:36:05 -0700

    putchar

---
+++ 0x0C-more_malloc_free/_putchar.c
@@ -0,0 +1,11 @@
+#incude "main.h"
+
+/**
+ * _putchar - writes character c to stdout
+ * @c - the character to be printed
+ * Return: on success 1
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
