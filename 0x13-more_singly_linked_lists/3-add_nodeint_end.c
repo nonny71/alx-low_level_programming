aa5667228c21643814eb695cef31e56cfd9c2cc5
nonny71 <your email>
2022-10-26 07:31:52 -0700

    commit

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -15,19 +15,21 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	new = malloc(sizeof(listint_t));
 
 	if (!new)
-		return (NULL);
-
+	{
 	new->n = n;
 	new->next = NULL;
+	}
+	else
+		return (NULL);
 
-	if (*head == NULL)
+	if (temp != NULL)
 	{
-		*head = new;
-		return (*head);
-
-	{
-		while (temp->next)
+		while (temp->next != NULL)
 			temp = temp->next;
 		temp->next = new;
-	return (*head);
+	}
+	else
+		*head = new;
+	return (new);
+
 }
