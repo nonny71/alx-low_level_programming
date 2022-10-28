d71a063e5ea445db4e3f44f296529923cb89002a
nonny71 <your email>
2022-10-27 17:29:39 -0700

    header

---
+++ 0x14-bit_manipulation/_putchar.c
@@ -0,0 +1,13 @@
+#define <unistd.h>
+#define "main.h"
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
