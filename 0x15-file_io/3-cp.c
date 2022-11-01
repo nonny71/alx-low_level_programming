03e3aab519c4934418970e8da353b7f8def43663
nonny71 <your email>
2022-10-31 17:58:57 -0700

    append

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -66,9 +66,9 @@ int main(int argc, char *argv[])
 	r = read(from, buffer, 1024);
 	to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 
-	do
-	{
+	do {
 		if (from == -1 || r == -1)
+		{
 			dprintf(STDERR_FILENO,
 					"Error: Can't read from file %s\n", argv[1]);
 		free(buffer);
@@ -84,7 +84,7 @@ int main(int argc, char *argv[])
 	r = read(from, buffer, 1024);
 	to = open(argv[2], O_WRONLY | O_APPEND);
 
-	while (r > 0);
+	} while (r > 0);
 	free(buffer);
 	close_file(from);
 	close_file(to);
