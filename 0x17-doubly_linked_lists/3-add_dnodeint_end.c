22b59f15e464424e1d000ed95dddca2e100313bc
nonny71 <michealnonny71@gmail.com>
2022-12-09 16:16:08 -0800

    wait

---
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,19 @@
+4-free_dlistint.c
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_dlistint - Frees a dlistint_t.
+ * @head: The head of the list to free.
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *tmp;
+
+	while (head != NULL)
+	{
+		tmp = head;
+		head = head->next;
+		free(tmp);
+	}
+}
