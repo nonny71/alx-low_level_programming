7a6fc11afe993ebecb216d5f4313de34a7064f83
nonny71 <michealnonny71@gmail.com>
2022-12-12 06:33:21 -0800

    putchar

---
+++ 0x18-dynamic_libraries/_putchar.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+* _putchar - writes character c to stdout
+* @c - the character to be printed
+* Return: on success 1
+*/
+
+int _putchar(char c)
+
+{
+	return (write(1, &c, 1));
+}
